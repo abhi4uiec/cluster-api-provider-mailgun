8f0a47bfe675dcb78c02c1e595da8bf5a6338694
abguptaakelius <112687192+abguptaakelius@users.noreply.github.com>
2022-11-01 04:49:37 +0200

    Initial Commit

---
+++ api/v1alpha3/mailguncluster_types.go
@@ -0,0 +1,78 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha3
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Priority string
+
+const (
+	// PriorityUrgent means do this right away
+	PriorityUrgent = Priority("Urgent")
+
+	// PriorityUrgent means do this immediately
+	PriorityExtremelyUrgent = Priority("ExtremelyUrgent")
+
+	// PriorityBusinessCritical means you absolutely need to do this now
+	PriorityBusinessCritical = Priority("BusinessCritical")
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MailgunClusterSpec defines the desired state of MailgunCluster
+type MailgunClusterSpec struct {
+	// Priority is how quickly you need this cluster
+	Priority Priority `json:"priority,omitempty"`
+	// Request is where you ask extra nicely
+	Request string `json:"request,omitempty"`
+	// Requester is the email of the person sending the request
+	Requester string `json:"requester,omitempty"`
+}
+
+// MailgunClusterStatus defines the observed state of MailgunCluster
+type MailgunClusterStatus struct {
+	// MessageID is set to the message ID from Mailgun when our message has been sent
+	MessageID *string `json:"response"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// MailgunCluster is the Schema for the mailgunclusters API
+type MailgunCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MailgunClusterSpec   `json:"spec,omitempty"`
+	Status MailgunClusterStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// MailgunClusterList contains a list of MailgunCluster
+type MailgunClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MailgunCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MailgunCluster{}, &MailgunClusterList{})
+}
---
+++ controllers/mailguncluster_controller.go
@@ -0,0 +1,107 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/cluster-api/util"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	infrastructurev1alpha3 "github.com/abhi4uiec/cluster-api-provider-mailgun/api/v1alpha3"
+	"github.com/go-logr/logr"
+	"github.com/mailgun/mailgun-go"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+)
+
+// MailgunClusterReconciler reconciles a MailgunCluster object
+type MailgunClusterReconciler struct {
+	client.Client
+	Scheme    *runtime.Scheme
+	Log       logr.Logger
+	Mailgun   mailgun.Mailgun
+	Recipient string
+}
+
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=mailgunclusters,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=mailgunclusters/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=mailgunclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the MailgunCluster object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
+func (r *MailgunClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	ctx = context.Background()
+	_ = r.Log.WithValues("mailguncluster", req.NamespacedName)
+
+	var mgCluster infrastructurev1alpha3.MailgunCluster
+	if err := r.Get(ctx, req.NamespacedName, &mgCluster); err != nil {
+		// 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	cluster, err := util.GetOwnerCluster(ctx, r.Client, mgCluster.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if cluster == nil {
+		return ctrl.Result{}, fmt.Errorf("failed to retrieve cluster: %+v", mgCluster.ObjectMeta)
+	}
+
+	if mgCluster.Status.MessageID != nil {
+		// We already sent a message, so skip reconcilation
+		return ctrl.Result{}, nil
+	}
+
+	if cluster == nil {
+	}
+
+	subject := fmt.Sprintf("[%s] New Cluster %s requested", mgCluster.Spec.Priority, cluster.Name)
+	body := fmt.Sprintf("Hello! One cluster please.\n\n%s\n", mgCluster.Spec.Request)
+
+	msg := mailgun.NewMessage(mgCluster.Spec.Requester, subject, body, r.Recipient)
+	_, msgID, err := r.Mailgun.Send(msg)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	mgCluster.Status.MessageID = &msgID
+	r.Update(ctx, &mgCluster)
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *MailgunClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&infrastructurev1alpha3.MailgunCluster{}).
+		Complete(r)
+}
